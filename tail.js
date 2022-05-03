4871808f9fb2497f60e9691bf99c9d694bc49820
Artem Novikov <novikov711@gmail.com>
2022-05-03 00:44:58 +0000

    Function to remove array tail

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let tail = function(elem) {
+  let array = elem;
+  array.shift();
+  return array;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.toString(), (["Lighthouse", "Labs"]).toString());
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
