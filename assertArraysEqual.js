8dd7b5b2175e21f2f80c6fb7bde19c69bc4e86ef
Artem Novikov <novikov711@gmail.com>
2022-05-04 20:57:16 +0000

    Changed function expression to a variable

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
-function eqArrays(a, b) {
+const eqArrays = function(a, b) {
   if (JSON.stringify(a) === JSON.stringify(b)) {
     return true;
   } else {
     return false;
   }
 }
-function assertArraysEqual(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1) === eqArrays(arr2)) {
     return true;
   } else {
