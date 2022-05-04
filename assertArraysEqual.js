9d04599b3111747b04090a4569640717944af592
Artem Novikov <novikov711@gmail.com>
2022-05-04 17:05:01 +0000

    Updated eqArray function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,7 +8,9 @@ function eqArrays(a, b) {
 function assertArraysEqual(arr1, arr2) {
   if (eqArrays(arr1) === eqArrays(arr2)) {
     return true;
-  } 
+  } else {
+    return false;
+  }
 }
 
-console.log(eqArrays([3,1],[3,2]));
+console.log(eqArrays([3,2],[3,2]));
