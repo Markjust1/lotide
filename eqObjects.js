9f24a7141ab5e1628ce0c0003233cbabd5a09c92
Artem Novikov <novikov711@gmail.com>
2022-05-05 02:40:01 +0000

    Check if two objects are the same

---
+++ eqObjects.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  let objKeys1 = JSON.stringify(Object.keys(object1));
+  let objValues1 = JSON.stringify(Object.values(object1));
+  let objKeys2 = JSON.stringify(Object.keys(object2));
+  let objValues2 = JSON.stringify(Object.values(object2));
+ 
+  if (objKeys1 === objKeys2 && objValues1 === objValues2) {
+    return true;
+  }
+};
+
+const ab = { a: '1', b: '2'};
+const ba = { a: '1', b: '2'};
+eqObjects(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc);
+
+console.log(assertEqual(eqObjects(ab, abc), true));
