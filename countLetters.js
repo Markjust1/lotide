d61086552961a489510b3635e6edc26b0757fad8
Artem Novikov <novikov711@gmail.com>
2022-05-05 00:16:16 +0000

    Counted the number of letters in a parameter

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(elem) {
+  let result = {};
+  for (let item of elem) {
+    if (item !== ' ') {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('This is sparta'));
