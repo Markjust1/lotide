7a889b01a2d92dd9822f543223bae515590e9dc4
Artem Novikov <novikov711@gmail.com>
2022-05-05 04:38:30 +0000

    Function to remove nested array within an array

---
+++ flatten.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(a, b) {
+  if (JSON.stringify(a) === JSON.stringify(b)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function flatten(elem) {
+  let output = [];
+  for (item of elem) {
+    if (Array.isArray(item) !== true) {
+      output.push(item);
+    } else {
+      for (elem of item) {
+        output.push(elem);
+      }
+    }
+  }
+  return output;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
