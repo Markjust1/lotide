00ffaa8d2788340e5d6484d1ddccc28284f4cc41
Artem Novikov <novikov711@gmail.com>
2022-05-06 01:40:16 +0000

    Looping through the array and stopping until the condition is met

---
+++ takeUntil.js
@@ -0,0 +1,36 @@
+const eqArrays = function(a, b) {
+  if (JSON.stringify(a) === JSON.stringify(b)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1) === eqArrays(arr2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const takeUntil = function(array, callback) {
+  let y = [];
+  for (item of array) {
+    if (!callback(item)){
+      y.push(item);
+    } else {
+      break;
+    }
+  }
+  return y;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
