608262b6333f8a4d92d951583b803b9d8e724692
Artem Novikov <novikov711@gmail.com>
2022-05-09 23:25:43 +0000

    added middle.js test cases for mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4,5]));
-
-//Bug found when using strings
-console.log(assertArraysEqual(middle([['apple', 'orange', 'grapefruit', 'lemon', 'papaya']]), ['grapefruit']));
+describe('#Middle' , () => {
+  it('returns [9, 10] for [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]', () => {
+    assert.deepEqual(middle([5, 6, 7, 8, 9, 10, 11, 12, 13, 14]) , [9, 10]);
+  });
+  it('returns [7] for [9, 8, 7, 6, 5]', () => {
+    assert.deepEqual(middle([9, 8, 7, 6, 5]) , [7]);
+  });
+  it('returns [8] for [2, 4, 8, 5, 1]', () => {
+    assert.deepEqual(middle([2, 4, 8, 5, 1]) , [8]);
+  });
+  it('returns [123, 7378] for [456, 94, 123, 7378, 1, 6]', () => {
+    assert.deepEqual(middle([456, 94, 123, 7378, 1, 6]) , [123, 7378]);
+  });
+  it('returns ["grapefruit"] for ["apple", "orange", "grapefruit", "lemon", "papaya"]])', () => {
+    assert.deepEqual(middle(["apple", "orange", "grapefruit", "lemon", "papaya"]) , ["grapefruit"]);
+  });
+});
