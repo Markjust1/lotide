80fd2965ccef69818e2f4249932a39d2e9a9c584
Artem Novikov <novikov711@gmail.com>
2022-05-09 21:01:36 +0000

    Refactored some code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,11 @@
 const eqArrays = require('./eqArrays');
 
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1) === eqArrays(arr2)) {
-    return true;
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return false;
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
 
-module.exports = eqArrays;
-
-
+module.exports = assertArraysEqual;
