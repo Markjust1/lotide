54e8b81987ac956e7af11703822491a92f687617
Artem Novikov <novikov711@gmail.com>
2022-05-09 23:37:25 +0000

    added tail.js test cases for mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,14 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const result = tail(["House", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
+describe('#Tail' , () => {
+  it('returns [6, 7, 8, 9, 10, 11, 12, 13, 14] for [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]', () => {
+    assert.deepEqual(tail([6, 7, 8, 9, 10, 11, 12, 13, 14]) , [7, 8, 9, 10, 11, 12, 13, 14]);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["House", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["House", "Lighthouse", "Labs"]) , ["Lighthouse", "Labs"]);
+  });
+  it('returns ["orange", "grapefruit", "lemon", "papaya"] for ["apple", "orange", "grapefruit", "lemon", "papaya"]])', () => {
+    assert.deepEqual(tail(["apple", "orange", "grapefruit", "lemon", "papaya"]) , ["orange", "grapefruit", "lemon", "papaya"]);
+  });
+});
